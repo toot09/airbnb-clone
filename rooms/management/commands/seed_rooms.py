000e23e3931d97444d4346239fcc591ad5471988
유재원 <ujone@yujaewon-ui-MacBookAir.local>
2021-01-24 02:05:01 +0900

    seed_rooms(Faker, random.randint)

--- rooms/management/commands/seed_rooms.py
+++ rooms/management/commands/seed_rooms.py
@@ -1,5 +1,6 @@
 import random
 from django.core.management.base import BaseCommand
+from django.contrib.admin.utils import flatten
 from django_seed import Seed
 from rooms import models as room_models
 from users import models as user_models
@@ -30,5 +31,15 @@ class Command(BaseCommand):
             "baths": lambda x : random.randint(1,3),
             #'is_staff':False,
         },)
-        seeder.execute()
+        # seeder.execute() return Primary Key
+        create_photos = seeder.execute()
+        #print(create_photos.values()) #2D Array returned
+        for pk in flatten(create_photos.values()):
+            room = room_models.Room.objects.get(pk=pk)
+            for i in range(3,random.randint(10,17)):
+                room_models.Photo.objects.create(
+                    caption=seeder.faker.sentence(),
+                    file=f"/room_photos/{random.randint(1,31)}.webp",
+                    room=room,
+                )
         self.stdout.write(self.style.SUCCESS(f"{number} rooms created!"))
