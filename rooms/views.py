2e378cd64f3a207a51ad272db4af2f66811b7400
ujone(Mac) <03toot28@gmail.com>
2021-02-04 21:27:55 +0900

    Handling Exception

--- rooms/views.py
+++ rooms/views.py
@@ -1,15 +1,18 @@
 from math import ceil
-from django.shortcuts import render
-from django.core.paginator import Paginator
+from django.shortcuts import render, redirect
+from django.core.paginator import Paginator, EmptyPage
 from . import models
 
 
 def all_rooms(request):
     # USE GET Method
-    page = request.GET.get("page")
+    page = request.GET.get("page",1)
     room_list = models.Room.objects.all()
 
     #Pagination (https://docs.djangoproject.com/en/2.2/topics/pagination/)
-    paginator = Paginator(room_list, 10)
-    rooms = paginator.get_page(page)
-    return render(request, "rooms/home.html", {"rooms" :rooms})
+    paginator = Paginator(room_list, 10, orphans=5)
+    try:
+        rooms = paginator.get_page(int(page))
+        return render(request, "rooms/home.html", {"page" :rooms})
+    except Exception:
+        return redirect("/")
