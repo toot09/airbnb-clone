7747b918e0d0081e76ea9787e93979a497692d9d
ujone(Mac) <03toot28@gmail.com>
2021-02-24 22:14:44 +0900

    Amenities and Faciliteis Form:

--- rooms/views.py
+++ rooms/views.py
@@ -53,17 +53,34 @@ def Search(request):
     city = str.capitalize(city)
     country = request.GET.get("country","KR")
     room_type = int(request.GET.get("room_type",0))
-    room_types = models.RoomType.objects.all()
-    
+    price = int(request.GET.get("price",0))
+    guests = int(request.GET.get("guests",0))
+    bedrooms = int(request.GET.get("bedrooms",0))
+    baths = int(request.GET.get("baths",0))
+    beds = int(request.GET.get("beds",0))
+    s_amenities = request.GET.get("amenity")
+    s_facilities = request.GET.get("facility")
     form = {
         "city":city, 
         "s_country":country, 
         "s_room_type":room_type,
+        "price":price,
+        "guests":guests,
+        "bedrooms":bedrooms,
+        "baths":baths,
+        "beds":beds,
+        "s_amenities":s_amenities,
+        "s_facilities":s_facilities,
     }
 
+    room_types = models.RoomType.objects.all()
+    amenities = models.Amenity.objects.all()
+    facilities = models.Facility.objects.all()
     choices = {
         "countries":countries, 
         "room_types":room_types,
+        "amenities":amenities,
+        "facilities":facilities,
     }
 
     return render(
